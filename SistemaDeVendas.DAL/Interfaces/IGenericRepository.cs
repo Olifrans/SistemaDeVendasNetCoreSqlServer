aa842324938ecd651b8410acb068da16b3cdfcf3
FRANCISCO OLIVEIRA <fransoliveira@gmail.com>
2023-10-10 18:23:16 -0300

    feat: Interface IGenericRepository<TEntity> where TEntity : class

---
+++ SistemaDeVendas.DAL/Interfaces/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace SistemaDeVendas.DAL.Interfaces
+{
+	public interface IGenericRepository<TEntity> where TEntity : class
+	{
+		Task<TEntity> Get(Expression<Func<TEntity, bool>> filtro);
+		Task<TEntity> Create(TEntity entity);
+		Task<bool> Update(TEntity entity);
+		Task<bool> Delete(TEntity entity);
+		Task<IQueryable> GetAll(Expression<Func<TEntity, bool>> filtro = null);
+	}
+}
